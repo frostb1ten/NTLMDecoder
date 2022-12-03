1c2f804256d1919f06d7554ea13f7ac692b37c37
Frostb1te <68353531+frostb1ten@users.noreply.github.com>
2022-12-03 10:59:19 -0600

    Update ntlmdecode.py

--- ntlmdecode.py
+++ ntlmdecode.py
@@ -1,9 +1,10 @@
 import hashlib
 import itertools
+import math
 import threading
 import time
 
-ntlm_hash = "8846F7EAEE8FB117AD06BDD830B7586C"
+ntlm_hash = "0CB6948805F797BF2A82807973B89537"
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"
 
 event = threading.Event()
@@ -17,16 +18,19 @@ def guess_password(password):
       event.set()
 
 start_time = time.time()
+
 threads = []
 
-for password in itertools.product(characters, repeat=8):
-   thread = threading.Thread(target=guess_password, args=(password,))
-   threads.append(thread)
-   thread.start()
-   if event.is_set():
-      for thread in threads:
-         thread.join()
-      break
+for length in range(1,13):
+   for password in itertools.product(characters, repeat=length):
+      thread = threading.Thread(target=guess_password, args=(password,))
+      threads.append(thread)
+      thread.start()
+      if event.is_set():
+         for thread in threads:
+            thread.join()
+         break
+
 
 end_time = time.time()
 elapsed_time = round(end_time - start_time)
